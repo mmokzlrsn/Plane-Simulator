01d554317b5e135445cc54ccc8bb3886a9a2c01a
Mehmet Ali KIZILARSLAN <mehmetali729@gmail.com>
2021-09-19 09:25:20 +0300

    Initial commit

---
+++ Plane Simulator/Plane Simulator CWC/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float forwardSpeed = 10.0f;
+    
+    private float verticalInput;
+    private float verticalRotateSpeed = 70.0f;
+
+
+    /*
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // This will move the plane to the forward so plane will not gonna stop
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+
+        // Taking inputs to rotate the plane up or down
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.right, verticalInput * Time.deltaTime * verticalRotateSpeed);
+
+      
+
+        
+    }
+}
